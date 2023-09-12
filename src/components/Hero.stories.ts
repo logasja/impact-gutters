1d27f36dd487dd76fee3978cb154b4b1faf76067
Jacob Logas <logasja@outlook.com>
2023-09-11 21:32:36 -0400

    Update storybook and add tagline function to Hero component

--- src/components/Hero.stories.ts
+++ src/components/Hero.stories.ts
@@ -19,7 +19,9 @@ type Story = StoryObj<typeof Hero>;
  */
 export const Primary: Story = {
   args: {
-    title: 'impact gutters',
-    heroimg: 'https://images.unsplash.com/photo-1518736346281-76873166a64a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    title: 'Company Name',
+    heroimg: 'https://images.unsplash.com/photo-1518736346281-76873166a64a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    tagline: 'Byline/Tagline',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   },
 };
